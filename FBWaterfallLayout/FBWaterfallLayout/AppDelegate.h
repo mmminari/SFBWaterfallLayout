0ec0ca40b7b6554520901aa21b04bc42f8d80b34
mmminari <mmminari@smtown.com>
2016-12-08 16:56:08 +0900

    commit

---
+++ FBWaterfallLayout/FBWaterfallLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FBWaterfallLayout
+//
+//  Created by 김민아 on 2016. 12. 8..
+//  Copyright © 2016년 김민아. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
